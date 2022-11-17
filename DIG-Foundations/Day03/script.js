1514a861e7172e2455d1211c42a285b26f39deca
平石進 <115196019+Susumu-Hiraishi@users.noreply.github.com>
2022-11-17 20:48:15 +0900

    Day03
    ナイトメアまで美味しく頂きました。

--- DIG-Foundations/Day03/script.js
+++ DIG-Foundations/Day03/script.js
@@ -1,6 +1,14 @@
 'use strict'
 // 1行目に記載している 'use strict' は削除しないでください
 
+let weight = 80;
+let hight = 1.75
+console.log(weight / (hight * hight))
+
+function calcBMI(weight,hight){
+    return weight / (hight * hight);
+}
+console.log("私の理想BMI= " + calcBMI(71,1.75))
 
 
 
@@ -47,19 +55,51 @@ console.log(isEqual(50, "50"));
 function isGreaterThan(valueOne, valueTwo) {
     console.log("5の答え = ",valueOne !== valueTwo);
 }
-isGreaterThan(50,40)
+isGreaterThan(50,40);
 //6
-
-
-
+function isOfAge(age){
+    return age >= 20;
+}
+console.log(isOfAge(35));
 
 
 //ここから　中級演習
 console.log("")
 console.log("ここから中級演習")
+// 先ほど作成した関数 isOfAge を「ブーリアンの値を返すだけではなく、飲める年齢なのかどうかを表す文字列をコンソールに表示する関数」になるよう修正しましょう
+function isOfAge1(age){
+    if (age >= 20){
+        return "お酒を飲んでよし！";
+    }else{
+        return "未成年でしょ！";
+    }
+}     
+console.log(isOfAge1(18));
 
+// 引数に数字 n をとる isEven という名前の関数を作成しましょう。引数が偶数であれば true を、そうでなければ false を返してください。
+function isEven(num){
+    let surplus = num % 2;
+    return surplus === 0;
+}
+console.log(isEven(24));
 
+// 2 つの引数 username と password をとる validCredentials という名前の関数を作成しましょう
+function validCredentials(username,password){
+    console.log("名前の文字数は=",username.length > 8);
+    console.log("PWの文字数は=",password > 8 );
+}
+validCredentials("Susumu Hiraishi","password")
 
 //ここから　ナイトメア
 console.log("")
-console.log("ここから中級演習")
+console.log("ここからナイトメア")
+
+function flatten(array1){
+    console.log("IN配列数=",array1.length)
+    let array2 = [array1.join()]
+    console.log("OUT配列数=",array2.length)
+    return array2
+}
+console.log(flatten([[1, 2, 3],[4, 5, 6],])); // [1, 2, 3, 4, 5, 6]
+console.log(flatten([1, 2, 3, [4, 5, 6]])); // [1, 2, 3, 4, 5, 6]
+console.log(flatten([[1], [2], [3], [4, 5, 6]])); // [1, 2, 3, 4, 5, 6]

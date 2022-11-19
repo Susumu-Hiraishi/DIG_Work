7d1bd86a98ea3fc2c81ab27cd619fa8024d8eb1b
Susumu1973 <90904800+Susumu1973@users.noreply.github.com>
2022-11-19 16:01:12 +0900

    Day05-1
    基礎

--- DIG-Foundations/Day05-1/script.js
+++ DIG-Foundations/Day05-1/script.js
@@ -4,9 +4,37 @@
 console.log("");
 console.log("＝ここから、ウォーミングアップ＝");
 
+// function isHot(temperature) {
+//     if temperature>30 {
+//     return "It's hot!";
+//     return "It's not hot.";
+// }
 
+// console.log(isHot(31)); // "It's hot!" を表示
+// console.log(isHot(2)); // "It's not hot." を表示
+// console.log(isHot(30)); // "It's not hot." を表示
 
+// function getTypeOfSchool(age) {
+//     if age < 6
+//     return "kindergarten";
+//     } if else (age < 11); {
+//           return "elementary school";
+//     else (age > 15) {
+//   return "junior high school";
+//     }
+//     if (age < 18) {
+//   return "high school";
+//     }
+//     else {
+//     return "after high school";
+//     }
+//   }
 
+// console.log(getTypeOfSchool(3)); // "kindergarten" を表示
+// console.log(getTypeOfSchool(8)); // "elementary school" を表示
+// console.log(getTypeOfSchool(13)); // "junior high school" を表示
+// console.log(getTypeOfSchool(16)); // "high school" を表示
+// console.log(getTypeOfSchool(30)); // "after high school" を表示
 
 
 
@@ -15,6 +43,33 @@ console.log("＝ここから、ウォーミングアップ＝");
 console.log("");
 console.log("＝ここから、基礎演習＝");
 
+// ファイル全体で使用する TDD 用の変数を宣言する
+let expected;   //期待値
+let actual;     //実際値
+
+//1
+/**
+ * @param {string} firstName - 下の名前
+ * @param {string} lastName - 苗字
+ * @returns {string} 名前と苗字を連結し、間にスペースを入れたもの
+ */
+
+ function getFullName(firstName, lastName) {
+    return firstName + " " + lastName;
+}
+   
+actual = getFullName("Ken", "Watanabe");
+expected = "Ken Watanabe";
+   
+if (actual === expected) {
+    console.log("Test PASSED!");
+} else {
+    console.error("Test FAILED. Keep trying!");
+    console.group("Result:");
+    console.log("  actual:", actual);
+    console.log("expected:", expected);
+    console.groupEnd();
+}
 
 
 

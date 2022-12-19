fd358a9d9670f387f552af8783dd4a009cfc3765
Susumu1973 <90904800+Susumu1973@users.noreply.github.com>
2022-12-20 06:11:39 +0900

    MemberFollow-2
    メンバーフォロー

---
+++ DIG-Member-Fllow/yamaguchi-san_moto/script.js
@@ -0,0 +1,9 @@
+const input = document.getElementById("input") ;
+
+//文章表示
+function textDisplay() {
+  const text1 = document.getElementsByTagName("h5")[0] ;
+  text1.style.display = "block" ;
+}
+
+input.addEventListener("click", textDisplay) ;

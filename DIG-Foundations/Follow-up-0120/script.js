b0a4a9b458de0d8b855feccf270b78737f4e2663
平石進 <115196019+Susumu-Hiraishi@users.noreply.github.com>
2023-01-20 17:40:46 +0900

    follow-up_20230120
    AMA　１／２０

---
+++ DIG-Foundations/Follow-up-0120/script.js
@@ -0,0 +1,59 @@
+'use strict';
+// please do not delete the 'use strict' line above
+
+document.getElementById('color-button').addEventListener('click', changeColor)
+
+//bodyのバックグランドカラーを変えるための初期設定
+let rColor = 0, gColor = 0, bColor = 0;
+let rgbColor = 'rgb(255,255,255)';
+document.body.style.backgroundColor = rgbColor;
+
+
+/**
+ * Changcolor を押された時の処理
+*/
+function changeColor() {
+  console.log('Button clicked!'); // feel free to change/delete this line
+  colorChg();
+  document.body.style.backgroundColor = rgbColor;
+
+}
+
+/**
+ * @returns {string<rgb(number,number,number)>} 　RGBカラーのランダム結果を返す関数
+*/
+function colorChg() {
+  rColor = Math.floor(Math.random() * (255 + 1 - 0));
+  gColor = Math.floor(Math.random() * (255 + 1 - 0));
+  bColor = Math.floor(Math.random() * (255 + 1 - 0));
+
+  console.log(`R= ${rColor} : G= ${gColor} : B= ${bColor} : `);
+  rgbColor = `rgb(${rColor}, ${gColor}, ${bColor})`;
+}
+
+//追加処理
+//<div>タグの追加
+let bodyElement1 = document.getElementsByTagName('body');
+let newElement1 = document.createElement('div');
+let buttonElement = document.getElementById('color-button');
+bodyElement1[0].insertBefore(newElement1, buttonElement);
+
+//ボタンをdivタグの中に移動
+let divElement = document.getElementsByTagName('div');
+divElement[0].appendChild(buttonElement);
+
+//<div>タグの中に<p>タグを追加
+let child1 = document.createElement('p');
+child1.append(document.createTextNode('FND-12 Yay!'));
+divElement[0].appendChild(child1);
+child1.style.backgroundColor = 'green';
+child1.style.color = 'yellow';
+child1.style.textAlign = 'center';
+child1.style.fontSize = '50px';
+
+//イメージを追加
+const ewElement2 = document.createElement('img');
+ewElement2.append(document.cre);
+bodyElement1[0].appendChild(ewElement2);
+ewElement2.id = 'img'
+ewElement2.src = 'https://user-images.githubusercontent.com/115196019/209415862-27b92d88-43c7-4267-8d24-a36363e14cf1.gif'
--- DIG-Foundations/Follow-up_3/script.js
+++ DIG-Foundations/Follow-up_3/script.js
@@ -3,6 +3,43 @@
 
 // test({ a: "A", b: "B" }, { b: "B", a: "A" });
 
+console.log("=1=");
 
-//ウォーミングアップ
-console.log("");
+// promptとalertを利用して、ログインを要求するコードを書いてください。
+// **promptとalertについてはグループでMDNを調べながら進めてもらっています。
+
+// 1.もし訪問者が サイトを訪問するとパスワードを要求されます。
+// 2.訪問者がパスワード(Tamaroh) を入力すると"welcome"と表示されます。
+// 3.訪問者がパスワードを間違えると、もう一度パスワードを要求します。
+// 4.訪問者がキャンセルをすると、"cancelled"と表示し、もう一度パスワードを要求します。
+
+
+// passwordを自由に決めたい！
+function setPW() {
+  let myPW= prompt("パスワードを入力してください。")
+  let cnt = 0;
+
+  function inputPassword() {
+    if (cnt < 3) {
+      console.log(cnt);
+      return;
+    }
+    
+    let sign = prompt("What's your password?");
+    // let password = "Tamaroh"
+    // 2.訪問者がパスワードを入力すると"welcome"と表示されます。
+    if (sign === myPW) {
+      alert("welcome");
+    }
+    // 3.訪問者がパスワードを間違えると、もう一度パスワードを要求します。
+    else {
+      cnt += 1;
+      alert("３回間違えました")
+      inputPassword();
+      }
+    }
+    return inputPassword();
+  }
+
+setPW();
+
